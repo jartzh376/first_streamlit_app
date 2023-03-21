d574ee300b2a70eac4ae6bacf6087a173637d051
jartzh376 <127449174+jartzh376@users.noreply.github.com>
2023-03-21 17:48:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('cantaloupe')
 streamlit.header(' moms  menu')
 
 
-#import pandas
+#import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
